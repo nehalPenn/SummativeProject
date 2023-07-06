1ea8d1345a05df481e23c1de71a33d8d51460552
notoriousnehal <nehalr@seas.upenn.edu>
2023-07-06 17:21:35 -0500

    magic test case

--- src/test/java/com/company/summativeproject/controller/MagicTest.java
+++ src/test/java/com/company/summativeproject/controller/MagicTest.java
@@ -1,50 +1,44 @@
 package com.company.summativeproject.controller;
 
+import com.company.summativeproject.models.Answer;
 import com.company.summativeproject.models.Quote;
-import org.junit.jupiter.api.Test;
-import com.company.summativeproject.models.Definition;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Arrays;
 import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest({WordController.class})
-public class DefinitionTest {
-
+@WebMvcTest({MagicController.class})
+public class MagicTest {
     @Autowired
     private MockMvc mockMvc;
     private ObjectMapper mapper = new ObjectMapper();
-    private List<Definition> defList;
+    private List<Answer> answerList;
 
-    public DefinitionTest(){}
+    public MagicTest () {}
 
     @BeforeEach
     public void setUp() {
     }
 
-    // Testing GET /quote
+    // Testing PUT /magic
     @Test
-    public void shouldReturnQuote() throws Exception {
-        // ARRANGE
-        // Convert Java object to JSON
-        String outputJson = mapper.writeValueAsString(defList);
+    public void shouldReturnRandomAnswer() throws Exception {
+        Answer question = new Answer();
+        question.setQuestion("Will I win the lottery?");
+        String inputJson = this.mapper.writeValueAsString(question);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/magic", new Object[0]).content(inputJson).contentType(MediaType.APPLICATION_JSON)).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isCreated());
 
-        // comment
-
-        // ACT
-        mockMvc.perform(get("/word"))                // Perform the GET request
-                .andDo(print())                          // Print results to console
-                .andExpect(status().isOk());              // ASSERT (status code is 200)
     }
 }
